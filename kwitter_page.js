bb12b62c071d0c01a32d5a71cbdbbe8bb61c6d23
monika83saini <117725305+monika83saini@users.noreply.github.com>
2022-11-08 15:30:56 +0530

    Add files via upload

---
+++ kwitter_page.js
@@ -0,0 +1,38 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBTVZ1Hsop9Jt8k0mHEXegYSRWoZS3cQnE",
+  authDomain: "igishka2-tssixm.firebaseapp.com",
+  databaseURL: "https://newagent-fhepbr.firebaseio.com",
+  projectId: "igishka2-tssixm",
+  storageBucket: "igishka2-tssixm.appspot.com",
+  messagingSenderId: "14896371054",
+  appId: "1:14896371054:web:fc4e1d55b42d5a7f41dd7f"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+//YOUR FIRE BASE LINKS
+
+	user_name = localStorage.getItem("user_name");
+	room_name = localStorage.getItem("room_name");
+
+function send()
+{
+  msg = document.getElementById("msg").value;
+  firebase.database().ref(room_name).push({
+    name:user_name,
+    message:msg,
+    like:0
+   });
+
+  document.getElementById("msg").value = "";
+}
+
+function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
+         firebase_message_id = childKey;
+         message_data = childData;
+//Start code
+         
+//End code
+      } });  }); }
+getData();
+
